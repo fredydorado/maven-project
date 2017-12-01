5d427bed67adf6434f9da6461bb4b4a272e89ac6
Fredy Dorado <fredyxdorado@gmail.com>
2017-11-30 19:12:48 -0500

    Se realizan sugerencias de checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,20 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     * Lets greet someone.
+     *
+     * @param someone someone to greet.
+     * @return A strings with the greeting.
+     */
+    public final String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+    }
 }
